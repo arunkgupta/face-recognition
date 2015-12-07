76a64d26c492e95a98ff94331f76363bdbfeaf28
hemanth <mphemanth@gmail.com>
2015-12-08 00:15:19 +0530

    added  live video capture

--- main.py
+++ main.py
@@ -1,38 +1,58 @@
 import cv2
 import sys
+import os
+import numpy as np
+from PIL import Image
 
-cascPath = sys.argv[1]
-faceCascade = cv2.CascadeClassifier(cascPath)
 
-video_capture = cv2.VideoCapture(0)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale(
-        gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-        minSize=(30, 30),
-        flags=cv2.CASCADE_SCALE_IMAGE
-    )
-
-    # Draw a rectangle around the faces
+def getImagesAndLabels(path):
+    global faceCascade
+    image_paths = [os.path.join(path, f) for f in os.listdir(path) if not (f.endswith('.sad') or f.endswith('.txt'))]
+    images=[]
+    labels=[]
+    for image_path in image_paths:
+        image_pil = Image.open(image_path).convert('L')
+        image = np.array(image_pil, 'uint8')
+        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", "")) 
+        faces = faceCascade.detectMultiScale(image)
+        for (x, y, w, h) in faces: 
+            images.append(image[y: y + h, x: x + w])
+            labels.append(nbr)
+            cv2.imshow("Adding faces to traning set...", image[y: y + h, x: x + w])
+            cv2.waitKey(50) 
+    return images,labels
+    
+def checkImages(path):
+    image_paths = [os.path.join(path, f) for f in os.listdir(path) if f.endswith('.sad') ]
+    predict_image_pil = Image.open(image_path).convert('L')
+    predict_image = np.array(predict_image_pil, 'uint8') 
+    faces = faceCascade.detectMultiScale(predict_image) 
     for (x, y, w, h) in faces:
-        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w]) 
+        nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", "")) 
+        if nbr_actual == nbr_predicted:
+            print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
+        else:
+            print "{} is Incorrectly Recognized as {}".format(nbr_actual, nbr_predicted)
+            cv2.imshow("Recognizing Face", predict_image[y: y + h, x: x + w])
+            cv2.waitKey(1000)
+        
+        
+def main():
+    global faceCascade
+    cascadePath = "haarcascade_frontalface_default.xml" 
+    faceCascade = cv2.CascadeClassifier(cascadePath)
+    images,laebls=getImagesAndLabels('../faces')
+    cv2.destroyAllWindows()
+    recognizer = cv2.createLBPHFaceRecognizer()
+    recognizer.train(images, np.array(labels))
+    checkImages('../faces')
+    
 
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-
-# When everything is done, release the capture
-video_capture.release()
-cv2.destroyAllWindows()
+if __name__=='__main__':
+    main()
 
 
 
